a2cd5f9711bc2821d5135231d2b57ef208420c74
Morten Jorgensen <morten00@gmail.com>
2017-11-06 17:49:03 +0100

    Ruby app updated to fit Rails MVC

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,54 @@
+require 'open-uri'
+require 'json'
+
+class GamesController < ApplicationController
+  def display
+    @grid = generate_grid(9)
+  end
+  def score
+    @result = run_game(params[:attempt].chars, params[:grid].chars, params[:start_time].to_time, Time.now)
+    @grid = params[:grid].chars
+    @attempt = params[:attempt].chars
+  end
+
+  def generate_grid(grid_size)
+    Array.new(grid_size) { ('A'..'Z').to_a.sample }
+  end
+
+  def included?(guess, grid)
+    guess.chars.all? { |letter| guess.count(letter) <= grid.count(letter) }
+  end
+
+  def compute_score(attempt, time_taken)
+    time_taken > 60.0 ? 0 : attempt.size * (1.0 - time_taken / 60.0)
+  end
+
+  def run_game(attempt, grid, start_time, end_time)
+    result = { time: end_time - start_time }
+
+    score_and_message = score_and_message(attempt, grid, result[:time])
+    result[:score] = score_and_message.first
+    result[:message] = score_and_message.last
+
+    result
+  end
+
+  def score_and_message(attempt, grid, time)
+    if included?(attempt.join.upcase, grid)
+      if english_word?(attempt.join)
+        score = compute_score(attempt, time)
+        [score, "well done"]
+      else
+        [0, "not an english word"]
+      end
+    else
+      [0, "not in the grid"]
+    end
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    return json['found']
+  end
+end
